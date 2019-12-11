6d19b68a88dd9d74902406e445c21ba5b8e63cd1
David Gomez <davdg25@gmail.com>
2019-12-11 16:18:00 -0700

    feature testing for pet show page, to have application links

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -11,15 +11,11 @@ RSpec.describe 'As A Visitor', type: :feature do
     @foxy = @dog_shelter.pets.create(image: 'https://image.pbs.org/video-assets/x1WLcZn-asset-mezzanine-16x9-6kkb4dA.jpg', name: 'Foxy', age: 5, sex: 'Female')
     @goblin = @cat_shelter.pets.create(image: 'https://static.boredpanda.com/blog/wp-content/uploads/2016/02/big-cute-eyes-cat-black-scottish-fold-gimo-1room1cat-fb__700-png.jpg', name: 'Goblin', age: 5, sex: 'Female')
 
-    @david_application = Application.create!(name: 'David', address: '4942 Willow Street', city: 'Denver', state: 'Colorado', zip: 80238, phone_number: 7209510554, description: 'I treat all animals well.')
-    @scott_application = Application.create!(name: 'Scott', address: '4942 willow street', city: 'denver', state: 'colorado', zip: 80238, phone_number: 1234567890, description: 'I should not be given this responsibility!')
-
-    @pet_application_1 = ApplicationPet.create!(pet_id: @elena.id, application_id: @david_application.id)
-    @pet_application_2 = ApplicationPet.create!(pet_id: @foxy.id, application_id: @david_application.id)
-    # binding.pry
-  end
-
-  it 'on favorites index page there is a list of pets with an application on them' do
+    # @david_application = Application.create!(name: 'David', address: '4942 Willow Street', city: 'Denver', state: 'Colorado', zip: 80238, phone_number: 7209510554, description: 'I treat all animals well.')
+    # @scott_application = Application.create!(name: 'Scott', address: '4942 willow street', city: 'denver', state: 'colorado', zip: 80238, phone_number: 1234567890, description: 'I should not be given this responsibility!')
+    #
+    # @pet_application_1 = ApplicationPet.create!(pet_id: @elena.id, application_id: @david_application.id)
+    # @pet_application_2 = ApplicationPet.create!(pet_id: @foxy.id, application_id: @david_application.id)
     visit "/pets/#{@elena.id}"
     click_on('Favorite!')
     visit "/pets/#{@foxy.id}"
@@ -49,6 +45,9 @@ RSpec.describe 'As A Visitor', type: :feature do
     click_on 'Submit Application'
 
     expect(current_path).to eq('/favorites')
+  end
+
+  it 'on favorites index page there is a list of pets with an application on them' do
 
     # expect(page).to have_content("Pet's with one or more application for Adoption!")
     # within "#application-#{@elena.id}" do
@@ -76,26 +75,39 @@ RSpec.describe 'As A Visitor', type: :feature do
     phone_number = 7209510554
     description = 'I will be amazing'
 
-    fill_in :name, with: name
-    fill_in :address, with: address
-    fill_in :city, with: city
-    fill_in :state, with: state
-    fill_in :zip, with: zip
-    fill_in :phone_number, with: phone_number
-    fill_in :description, with: description
-    find("#checkbox-#{@elena.id}").set(true)
-    find("#checkbox-#{@foxy.id}").set(true)
-
-    click_on 'Submit Application'
-
-    visit "/applications/#{@david_application.id}"
-
-    expect(page).to have_content(@david_application.name)
-    expect(page).to have_content(@david_application.address)
-    expect(page).to have_content(@david_application.city)
-    expect(page).to have_content(@david_application.state)
-    expect(page).to have_content(@david_application.zip)
-    expect(page).to have_content(@david_application.phone_number)
-    expect(page).to have_content(@david_application.description)
+    # fill_in :name, with: name
+    # fill_in :address, with: address
+    # fill_in :city, with: city
+    # fill_in :state, with: state
+    # fill_in :zip, with: zip
+    # fill_in :phone_number, with: phone_number
+    # fill_in :description, with: description
+    # find("#checkbox-#{@elena.id}").set(true)
+    # find("#checkbox-#{@foxy.id}").set(true)
+    #
+    # click_on 'Submit Application'
+    #
+    # visit "/applications/#{@david_application.id}"
+    #
+    # expect(page).to have_content(@david_application.name)
+    # expect(page).to have_content(@david_application.address)
+    # expect(page).to have_content(@david_application.city)
+    # expect(page).to have_content(@david_application.state)
+    # expect(page).to have_content(@david_application.zip)
+    # expect(page).to have_content(@david_application.phone_number)
+    # expect(page).to have_content(@david_application.description)
   end
+
+  it 'visit a specific pet show page i see links to all applications for that pet' do
+    visit "pets/#{@elena.id}"
+    expect(page).to have_link('David Gomez')
+  end
+#   User Story 20, Pet Applications Index Page
+#
+# As a visitor
+# When I visit a pets show page
+# I see a link to view all applications for this pet
+# When I click that link
+# I can see a list of all the names of applicants for this pet
+# Each applicant's name is a link to their application show page
 end
