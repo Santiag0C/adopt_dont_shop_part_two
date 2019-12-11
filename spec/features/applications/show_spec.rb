20a7a5e0173cc6d99a0eab763db1ddac381b48ea
David Gomez <davdg25@gmail.com>
2019-12-11 15:40:22 -0700

    create feature testing for /applications/id

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -11,8 +11,12 @@ RSpec.describe 'As A Visitor', type: :feature do
     @foxy = @dog_shelter.pets.create(image: 'https://image.pbs.org/video-assets/x1WLcZn-asset-mezzanine-16x9-6kkb4dA.jpg', name: 'Foxy', age: 5, sex: 'Female')
     @goblin = @cat_shelter.pets.create(image: 'https://static.boredpanda.com/blog/wp-content/uploads/2016/02/big-cute-eyes-cat-black-scottish-fold-gimo-1room1cat-fb__700-png.jpg', name: 'Goblin', age: 5, sex: 'Female')
 
-    # @david_application = Application.create!(name: 'David', address: '4942 Willow Street', city: 'Denver', state: 'Colorado', zip: 80238, phone_number: 7209510554, description: 'I treat all animals well.')
-    # @scott_application = Application.create!(name: 'Scott', address: '4942 willow street', city: 'denver', state: 'colorado', zip: 80238, phone_number: 1234567890, description: 'I should not be given this responsibility!')
+    @david_application = Application.create!(name: 'David', address: '4942 Willow Street', city: 'Denver', state: 'Colorado', zip: 80238, phone_number: 7209510554, description: 'I treat all animals well.')
+    @scott_application = Application.create!(name: 'Scott', address: '4942 willow street', city: 'denver', state: 'colorado', zip: 80238, phone_number: 1234567890, description: 'I should not be given this responsibility!')
+
+    @pet_application_1 = ApplicationPet.create!(pet_id: @elena.id, application_id: @david_application.id)
+    @pet_application_2 = ApplicationPet.create!(pet_id: @foxy.id, application_id: @david_application.id)
+    # binding.pry
   end
 
   it 'on favorites index page there is a list of pets with an application on them' do
@@ -54,11 +58,44 @@ RSpec.describe 'As A Visitor', type: :feature do
     #   expect(page).to have_link(@foxy.name)
     # end
   end
+
+  it 'Visit /applications/:id you can see all application information with links to every pet' do
+    visit "/pets/#{@elena.id}"
+    click_on('Favorite!')
+    visit "/pets/#{@foxy.id}"
+    click_on('Favorite!')
+    visit '/favorites'
+    click_on('Adopt A Pet')
+    expect(current_path).to eq('/applications/new')
+
+    name = 'David Gomez'
+    address = '4942 Willow Street'
+    city = 'Denver'
+    state = 'Colorado'
+    zip = 80238
+    phone_number = 7209510554
+    description = 'I will be amazing'
+
+    fill_in :name, with: name
+    fill_in :address, with: address
+    fill_in :city, with: city
+    fill_in :state, with: state
+    fill_in :zip, with: zip
+    fill_in :phone_number, with: phone_number
+    fill_in :description, with: description
+    find("#checkbox-#{@elena.id}").set(true)
+    find("#checkbox-#{@foxy.id}").set(true)
+
+    click_on 'Submit Application'
+
+    visit "/applications/#{@david_application.id}"
+
+    expect(page).to have_content(@david_application.name)
+    expect(page).to have_content(@david_application.address)
+    expect(page).to have_content(@david_application.city)
+    expect(page).to have_content(@david_application.state)
+    expect(page).to have_content(@david_application.zip)
+    expect(page).to have_content(@david_application.phone_number)
+    expect(page).to have_content(@david_application.description)
+  end
 end
-# User Story 18, List of Pets that have applications on them
-#
-# As a visitor
-# After one or more applications have been created
-# When I visit the favorites index page
-# I see a section on the page that has a list of all of the pets that have at least one application on them
-# Each pet's name is a link to their show page
