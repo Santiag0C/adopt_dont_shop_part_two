c7d7a8961b17356b495fcd057508a99323c0b184
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-12-12 09:03:53 -0700

    adds test for the application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :favorites
+
+  helper_method :favorites, :keys_to_i, :favorited_pets
+
   def favorites
     @favorites ||= Favorite.new(session[:favorites])
+  end
+
+  def keys_to_i(contents)
+    pet_id_int = []
+    contents.each do |pet_id, quantity|
+      pet_id_int.push(pet_id.to_i)
+    end
+    pet_id_int
+  end
+
+  def favorited_pets
+    pet_id = keys_to_i(favorites.contents)
+
+    @pets = []
+    pet_id.each do |id|
+      @pets.push(Pet.find(id))
+    end
+    @pets
   end
 end
