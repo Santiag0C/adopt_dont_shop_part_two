e5d4a6f791a157cc3d851f9c212c248e0b1515cf
David Gomez <davdg25@gmail.com>
2019-12-11 20:42:13 -0700

    Add applicattion objects so that testing can recognize Application

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -10,6 +10,9 @@ RSpec.describe 'As A Visitor when I go to /favorites', type: :feature do
     @raccoon = @raccoon_shelter.pets.create(image: 'https://imgix.ranker.com/user_node_img/50070/1001387908/original/1-photo-u1?w=650&q=50&fm=pjpg&fit=crop&crop=faces', name: 'Racccoon', age: 5, sex: 'Male')
     @foxy = @dog_shelter.pets.create(image: 'https://image.pbs.org/video-assets/x1WLcZn-asset-mezzanine-16x9-6kkb4dA.jpg', name: 'Foxy', age: 5, sex: 'Female')
     @goblin = @cat_shelter.pets.create(image: 'https://static.boredpanda.com/blog/wp-content/uploads/2016/02/big-cute-eyes-cat-black-scottish-fold-gimo-1room1cat-fb__700-png.jpg', name: 'Goblin', age: 5, sex: 'Female')
+
+    @david_application = Application.create!(name: 'David', address: '4942 Willow Street', city: 'Denver', state: 'Colorado', zip: 80238, phone_number: 7209510554, description: 'I treat all animals well.')
+    @scott_application = Application.create!(name: 'Scott', address: '4942 willow street', city: 'denver', state: 'colorado', zip: 80238, phone_number: 1234567890, description: 'I should not be given this responsibility!')
   end
 
   it 'all pets that I have favorited have a link to their show page via name and an image ' do
